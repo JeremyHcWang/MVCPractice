22330260dde4eef679845401d46d29001b2c5661
JeremyHcWang <jemhw9758@gmail.com>
2024-03-07 23:52:24 -0500

    Initial commit

---
+++ ApplicationCore/Entities/Shipper.cs
@@ -0,0 +1,8 @@
+namespace ApplicationCore.Entities;
+
+public class Shipper
+{   
+    public int Id { get; set; }
+    public string CompanyName { get; set; }
+    public int Phone { get; set; }
+}
